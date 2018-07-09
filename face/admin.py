6e5f24c6a57b26f246b97601fb026b259f6da288
Chinseok Lee <me@askcompany.kr>
2018-07-09 19:08:21 +0900

    기본모델로서 Collection/Person/Face 생성

--- face/admin.py
+++ face/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Collection, Person, Face
 
-# Register your models here.
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['collection', 'name']
+
+
+@admin.register(Face)
+class FaceAdmin(admin.ModelAdmin):
+    list_display = ['person', 'meta']
+
--- face/models.py
+++ face/models.py
@@ -1,3 +1,26 @@
+from django.core.validators import RegexValidator
 from django.db import models
+from jsonfield import JSONField
 
-# Create your models here.
+
+class Collection(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(validators=[RegexValidator(r'^[a-zA-Z0-9_\.-]+$')])
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Face(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    photo = models.ImageField()
+    meta = JSONField()
+
